6362d98d8d6e26c6a5823f7915bfa267a3fc3adc
vaibhavgaur1206 <vg1548@srmist.edu.in>
2026-01-28 23:19:14 +0530

    employeeApi get by Id

---
+++ EmployeeApi/EmployeeApi/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+    }
+}
